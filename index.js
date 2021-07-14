f31183d94188faf018d9798715af6cc0a82b29e5
danieldeandradelopes <danieldeandradelopes@gmail.com>
2021-07-14 16:06:41 -0300

    create test to sum

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ function sub(a, b) {
 function sum(a, b) {
   return a + b;
 }
+
+function testSum() {
+  const a = 10;
+  const b = 10;
+
+  const result = sum(a, b);
+
+  if (result === 20) {
+    return true;
+  }
+
+  return false;
+}
